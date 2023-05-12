501856fd3f675b368474283764b771b2ee1b23d7
Ethan Rudy <16.erudy@gmail.com>
2023-05-11 21:57:35 -0700

    Update NumHelper.hpp
    Moved the factors function to this file
    factors() function how takes a reference to a vector
    Added numOfFacts() function

--- Custom Headers/NumHelper.hpp
+++ Custom Headers/NumHelper.hpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <vector>
 
 bool palindrome(long long n) {
 	std::string rStr = std::to_string(n);
@@ -6,4 +7,20 @@ bool palindrome(long long n) {
 	std::reverse(rStr.begin(), rStr.end());
 
 	return (nStr == rStr);
+}
+
+void factors(long long n, std::vector<int>& factVec) {
+	for (long long i = 1; i <= n; ++i) {
+		if (n % i == 0) {
+			factVec.push_back(i);
+		}
+	}
+}
+
+int numOfFacts(long long n) {
+	int facts = 0;
+	for (int i = 1; i * i < n; ++i) {
+		if (n % i == 0) { facts += 2; }
+	}
+	return facts;
 }
